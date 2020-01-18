1fcebbd56f0ce1b99d7879168bbe1f28dca13f03
worthp <worthp@worthconsulting.com>
2020-01-18 08:50:12 -0500

    added PortMan to Climber

--- src/main/java/frc/robot/subsystem/climber/Climber.java
+++ src/main/java/frc/robot/subsystem/climber/Climber.java
@@ -2,8 +2,15 @@ package frc.robot.subsystem.climber;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import frc.robot.subsystem.PortMan;
+
 public class Climber {
+    
     private final TalonSRX leftIntake = new TalonSRX(57);
+
+    public void init(PortMan portMan) throws Exception {
+
+    }
     
     public void extend(){
 
