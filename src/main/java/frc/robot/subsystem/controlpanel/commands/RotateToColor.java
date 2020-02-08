8c553f8f1781c76fc07d961c94f08c9b669ee9da
Rahul <rahul@Rahair.local>
2020-02-08 09:20:18 -0500

    fixed RotateToColor

--- src/main/java/frc/robot/subsystem/controlpanel/commands/RotateToColor.java
+++ src/main/java/frc/robot/subsystem/controlpanel/commands/RotateToColor.java
@@ -41,7 +41,7 @@ public class RotateToColor extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    controlPanel.goToColor(targetColor);
+    controlPanel.testSensor();
   }
 
   // Called once the command ends or is interrupted.
