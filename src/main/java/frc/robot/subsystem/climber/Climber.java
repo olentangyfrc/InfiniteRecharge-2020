c01a656b5071eca2e9f7e501ab54cfe8118f9399
Rahul <rahul@Rahair.local>
2020-02-10 16:52:20 -0500

    added climber

--- src/main/java/frc/robot/subsystem/climber/Climber.java
+++ src/main/java/frc/robot/subsystem/climber/Climber.java
@@ -12,12 +12,28 @@ public class Climber extends SubsystemBase {
     private TalonSRX motor;
     
     public void init(PortMan portMan) throws Exception {
+        logger.info("init");
         motor = new TalonSRX(portMan.acquirePort(PortMan.can_47_label, "Climber.motor"));
 
+      motor.enableCurrentLimit(true);
+      motor.configPeakCurrentLimit(30);
+      motor.configContinuousCurrentLimit(20);
+      motor.configPeakCurrentDuration(400);
+      motor.configAllowableClosedloopError(0, 5);
+      motor.setSelectedSensorPosition(0, 0, 0);
+
+      motor.config_kP(0, .3, 0);
+      motor.config_kI(0, 0, 0);
+      motor.config_kD(0, .1, 0);
+      motor.config_kF(0, 0, 0);
+
+      motor.configMotionCruiseVelocity(4500);
+      motor.configMotionAcceleration(4096);
     }
     
     public void extend(){
         logger.info("extend");
+        
     }
 
     public void retract(){
