9be9fd611448e671cd2ea508605f064765511052
Eliott Barden <barden.e23@gmail.com>
2020-02-14 16:30:39 -0500

    Created Intake subsystem, added basic methods.

---
+++ src/main/java/frc/robot/subsystem/intake/IntakeSBTab.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystem.intake;
+
+import java.util.logging.Logger;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import frc.robot.subsystem.SBInterface;
+
+public class IntakeSBTab implements SBInterface{
+    private Intake intake;
+    private ShuffleboardTab tab;
+
+    private NetworkTableEntry gateStatus;
+
+
+    private static Logger logger = Logger.getLogger(IntakeSBTab.class.getName());
+
+    public IntakeSBTab(Intake i) {
+        
+        intake = i;
+
+        tab = Shuffleboard.getTab("Intake");
+
+        gateStatus = tab.add("Gate Up", false).getEntry();
+    }
+
+
+    /**
+     * get data from subsystem and update SB widgets
+     * get data from SB widgets and update subsystem
+     */
+    public void update() {
+        gateStatus.setBoolean(intake.getGateStatus());
+    }
+}
